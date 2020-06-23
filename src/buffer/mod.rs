25961c391cdf36478bbe75219824f1a28cc0583c
g-w1 <jacoblevgw@gmail.com>
2020-06-23 08:09:55 -0400

    gridding works

--- src/buffer/mod.rs
+++ src/buffer/mod.rs
@@ -14,29 +14,20 @@ impl Buffer {
     pub fn get_grid(&self, width: usize) -> Vec<String> {
         let mut result: Vec<String> = Vec::new();
         for i in self.content.iter() {
-            if i.as_str().len() > width {
-                let tuple_of_strs = i.as_str().split_at(width);
-                result.push(tuple_of_strs.0.to_string());
-                result.push(tuple_of_strs.1.to_string());
-            } else {
-                result.push(i.to_owned());
-            }
+            result.append(&mut Buffer::recsplit(i.to_string(), width));
         }
         result
     }
 
-    fn recsplit(thing_to_split: String) -> Vec<String> {
-            let mut result: Vec<String> = Vec::new();
-            if thing_to_split.as_str().len() > width {
-                let tuple_of_strs = i.as_str().split_at(width);
-                result.push(tuple_of_strs.0.to_string());
-                result.push(tuple_of_strs.1.to_string());
-            } else {
-                result.push(i.to_owned());
-            }
+    fn recsplit(thing_to_split: String, width: usize) -> Vec<String> {
+        let mut result: Vec<String> = Vec::new();
+        if thing_to_split.as_str().len() > width {
+            let tuple_of_strs = thing_to_split.as_str().split_at(width);
+            result.push(tuple_of_strs.0.to_string());
+            result.append(&mut Buffer::recsplit(tuple_of_strs.1.to_string(), width));
+        } else {
+            result.push(thing_to_split.to_owned());
         }
         result
-
-
     }
 }
