1515c97df8918e6ee088d18f8846d7ccf9a46fd5
g-w1 <jacoblevgw@gmail.com>
2020-06-23 12:59:51 -0400

    working read lines stdin

--- src/io/key_getter.rs
+++ src/io/key_getter.rs
@@ -1,12 +1,16 @@
 use crate::buffer::Buffer;
-use std::io::{Read, stdout, Write};
+use std::io::{stdin, stdout, Read, Write};
 use termion;
-use termion::async_stdin;
 use termion::event::Key;
+use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
-pub fn next_io() -> Key {
+pub fn next_key() -> Option<Key> {
     let mut stdout = stdout().into_raw_mode().unwrap();
-    let mut stdin = async_stdin().bytes();
-    let b = stdin.next();
+    let mut stdin = stdin();
+    for c in stdin.keys() {
+        // write!(stdout, "{}{}", termion::cursor::Goto(1,1), termion::clear::CurrentLine).unwrap();
+        return Some(c.unwrap());
+    }
+    None
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
 pub mod buffer;
 pub mod io;
+use termion::event::Key;
 fn main() {
     let string = "tesaoiesntraoinstaoisrentoarient aorsiten aositn       oinoin   \ntest\ntea";
     let buff = buffer::Buffer::new_from_string(string);
     for i in buff.get_grid(10).iter() {
         println!("{}", i);
+    }
+    match io::key_getter::next_key().unwrap() {
+        Key::Char(c) => println!("{}", c),
+        Key::Alt(c) => println!("*{}", c),
+        Key::Ctrl(c) => println!("^{}", c),
+        Key::Esc => println!("ESC"),
+        Key::Left => println!("Left"),
+        Key::Right => println!("Right"),
+        Key::Up => println!("Up"),
+        Key::Down => println!("Down"),
+        Key::Backspace => println!("Backspace"),
+        _ => {}
     }
 }
